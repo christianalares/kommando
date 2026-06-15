05c2d141b81b3a51cf3f8cda3c0d88934319424d
Christian Alares <christian.alares@gmail.com>
2026-06-15 06:49:00 +0200

    Initial Commit

---
+++ Kommando/KommandoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KommandoApp.swift
+//  Kommando
+//
+//  Created by Christian Alares on 2026-06-15.
+//
+
+import SwiftUI
+
+@main
+struct KommandoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
